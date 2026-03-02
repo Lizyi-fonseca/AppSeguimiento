8cbc2ce3bd2eff58e3cbd1669df005c707924508
Lizyi-fonseca <lizyinelafonsecarodriguez@gmail.com>
2026-03-02 15:26:40 -0500

    hize cambie aprendiceController

--- app/Http/Controllers/AprendicesController.php
+++ app/Http/Controllers/AprendicesController.php
@@ -2,9 +2,65 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\aprendices;
 use Illuminate\Http\Request;
 
 class AprendicesController extends Controller
 {
-    
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $aprendices = aprendices::all();
+        return view('Aprendices.index', compact('aprendices'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
 }
--- app/Http/Controllers/BitacoraController.php
+++ app/Http/Controllers/BitacoraController.php
@@ -7,26 +7,19 @@ use Illuminate\Http\Request;
 
 class BitacoraController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
         $bit = bitacora::all();
         return view('Bitacoras.index', compact('bit'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+
     public function create()
     {
         return view('Bitacoras.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $request ->validate([
@@ -62,35 +55,26 @@ class BitacoraController extends Controller
 
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(bitacora $bitacora)
     {
-        //
+        
+    
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(bitacora $bitacora)
     {
-        //
+    
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+
     public function update(Request $request, bitacora $bitacora)
     {
-        //
+      
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+
     public function destroy(bitacora $bitacora)
     {
-        //
+        
     }
 }
